4f1c785847acbe6539cd9d9a7624332d9be52005
Arash Alghasi <arash.alqasi@gmail.com>
2025-04-21 22:55:22 +0200

    mongo client with singleton

--- src/main/java/org/arash/Main.java
+++ src/main/java/org/arash/Main.java
@@ -1,7 +1,23 @@
 package org.arash;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println( "Connecting to mongodb" );
+        ConnectionString connectionString = new ConnectionString("mongodb+srv://arash:arash@firstcluster.fjeectn.mongodb.net/?retryWrites=true&w=majority&appName=FirstCluster");
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .serverApi(ServerApi.builder()
+                        .version(ServerApiVersion.V1)
+                        .build())
+                .build();
+        MongoClient mongoClient = MongoClients.create(settings);
     }
 }
---
+++ src/main/java/org/arash/MongoClientCustom.java
@@ -0,0 +1,32 @@
+package org.arash;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+public class MongoClientCustom {
+    private static MongoClient mongoClient;
+
+    private static synchronized MongoClient getMongoClient()
+    {
+        if (mongoClient == null)
+        {
+            System.out.println( "Creating a mongo client singleton" );
+            ConnectionString connectionString = new ConnectionString("mongodb+srv://arash:arash@firstcluster.fjeectn.mongodb.net/" +
+                    "?retryWrites=true&w=majority&appName=FirstCluster");
+            MongoClientSettings settings = MongoClientSettings.builder()
+                    .applyConnectionString(connectionString)
+                    .serverApi(
+                            ServerApi.builder()
+                                    .version(ServerApiVersion.V1)
+                                    .build()
+                    )
+                    .build();
+            MongoClient mongoClient = MongoClients.create(settings);
+        }
+        return mongoClient;
+    }
+}
